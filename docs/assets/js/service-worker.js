ed170ff836790e964418d55da7cc230b3dc16451
Ajay Ramesh <ajaycs14@gmail.com>
2021-10-15 20:26:12 -0500

    blog: remove todo

--- docs/assets/js/service-worker.js
+++ docs/assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.1.2--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2021-10-16T01:23:20+00:00--sw/";
+const CONTENT_CACHE = "content--2021-10-16T01:25:54+00:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
